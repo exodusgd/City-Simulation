c053266c7054361d4c01193000f2e7ade98d0e92
Exodus09 <migdinism@gmail.com>
2023-01-04 15:55:28 +0000

    Collisions and Movement Integration
    - Added collisions to the map and the "Player"
    - Integrated movement logic to move the existing camera

--- City-Simulation/Assets/Scripts/PlayerMovement.cs
+++ City-Simulation/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController controller;
+   public CharacterController controller;
    
    public float speed = 12f;
    public float gravity = -9.81f;
